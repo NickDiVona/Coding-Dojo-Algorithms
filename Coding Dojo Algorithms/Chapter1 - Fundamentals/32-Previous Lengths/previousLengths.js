10a4464d8971d3597d32b2a79a28163242dd4a86
Nick DiVona <skatedivona@gmail.com>
2017-07-16 01:59:32 -0700

    Clean up files

--- Coding Dojo Algorithms/Chapter1 - Fundamentals/32-Previous Lengths/previousLengths.js
+++
@@ -1,7 +0,0 @@
-function previousLengths(array) {
-    for (var i = array.length-1; i > 0; i--) {
-            array[i] = (array[i - 1]).length;
-    }
-    console.log(array);
-}
-previousLengths(["there","is","no","cow","level"])
--- Coding Dojo Algorithms/Chapter1 - Fundamentals/43-Soaring IQ/soaringIQ.js
+++
@@ -1,9 +0,0 @@
-function soaringIQ() {
-    var currentIQ = 101;
-
-    for (var i = .01; i <= .98; i += .01) {
-        currentIQ += i;
-    }
-    console.log(`IQ at end of bootcamp is ${currentIQ}.`);
-}
-soaringIQ()
