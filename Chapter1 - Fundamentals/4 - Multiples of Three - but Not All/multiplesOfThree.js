9a31ffb5c0bbf764e12af82c45c0b2d283cc3674
Nick DiVona <skatedivona@gmail.com>
2017-11-07 12:37:06 -0800

    remove node modules ignore directory node_modules

--- Chapter1 - Fundamentals/4 - Multiples of Three - but Not All/multiplesOfThree.js
+++ Chapter1 - Fundamentals/4 - Multiples of Three - but Not All/multiplesOfThree.js
@@ -1,16 +1,9 @@
-for (var i = -300; i <= 0; i+=3) {
-    if (i === -6 || i === -3) {
-        continue;
-    }
-    else {
-        console.log(i);
-    }
+// using FOR, print multiples of 3 from -300 to 0, skip -3 and -6.
+
+for (let i = -300; i <= 0; i += 3) {
+  if (i === -6 || i === -3) {
+    continue;
+  } else {
+    console.log(i);
+  }
 }
-
-/*
-    Line 1 sets up the initial for loop to iterate through all numbers from the minimum to the maximum by 3.
-
-    Line 2-3 are a check to see if there is a specific value, don't log anything.
-
-    Line 5-6 are the other part of the check. If the number isn't screened by the 'if' then this runs. In this case, it logs the value.
-*/
