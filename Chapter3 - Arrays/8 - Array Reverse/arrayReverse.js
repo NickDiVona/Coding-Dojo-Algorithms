4f34a169515d909ebf259b82752cc92d9f37511c
Nick DiVona <skatedivona@gmail.com>
2020-09-30 14:52:42 -0700

    complete reverse start rotate

---
+++ Chapter3 - Arrays/8 - Array Reverse/arrayReverse.js
@@ -0,0 +1,20 @@
+//given a numerical array, reverse the order of values in place
+
+let testArrEven = [1, 2, 3, 4, 5, 6];
+let testArrOdd = [11, 12, 13, 14, 15];
+
+function reverse(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+
+    // let mirrorVal = arr[arr.length - i + 1]
+
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
+  }
+
+  return arr;
+}
+
+console.log(reverse(testArrEven));
+console.log(reverse(testArrOdd));
